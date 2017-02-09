255bedd3a8bf28fc7bd2aa29818d399bd19d6ccc
Dave Hubertus <davehubertus@gmail.com>
2017-02-09 13:56:31 -0700

    reorganized js file so global vars and functions are at top

--- linkedlist.js
+++ linkedlist.js
@@ -2,7 +2,20 @@ var $webTitle = $('#web-title');
 var $webUrl = $('#web-url');
 var $submit = $('.submit');
 
+
 $('#submit').prop('disabled', true);
+
+
+function disableSubmit() {
+  $('#submit').prop('disabled', true);
+}
+
+
+function clearField() {
+    $webTitle.val("");
+    $webUrl.val("");
+}
+
 
 $('.submit').on('click', function(){
 
@@ -15,29 +28,23 @@ $('.submit').on('click', function(){
       '<button class="delete">Delete</button>' +
     '</article>'
   );
+
   clearField();
   disableSubmit();
+
 })
 
-function disableSubmit() {
-  $('#submit').prop('disabled', true);
-}
-
-function clearField() {
-  {
-    $webTitle.val("");
-    $webUrl.val("");
-  }
-}
 
 $('.bookmarks').on('click', '.read', function(){
   $(this).toggleClass('active');
   $(this).parent('.entry').toggleClass('activebackground');
 })
 
+
 $('.bookmarks').on('click', '.delete', function(){
   $(this).parent('.entry').remove();
 })
+
 
 $('input[type=text]').on('keyup', function () {
   if ($webTitle.val() !== "" && $webUrl.val() !== ""){
